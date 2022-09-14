cb67ca78dd0cd40280d5d14d6f442983b9c302c6
Lúcio Brito <luciopereirabritoo@gmail.com>
2022-09-14 00:11:39 -0300

    adicionado componente início

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,13 +12,15 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
