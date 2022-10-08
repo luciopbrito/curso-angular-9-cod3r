43f6a2eb87899015c2298ee7d2bf636e8bfd482e
Lucio Brito <luciopereirabritoo@gmail.com>
2022-10-08 00:55:06 -0300

    criado a função com observable para inserir um produto no back

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,6 +23,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,7 +46,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,20 +1,28 @@
 import { Injectable, Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './product.model';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  msg: string;
-  constructor(private snackBar: MatSnackBar) { 
-    this.msg = '';
+
+  baseUrl: string = "http://localhost:3001/products";
+
+  constructor(private snackBar: MatSnackBar,
+    private http: HttpClient) {
   }
 
   showMessage(msg: string): void {
-    this.msg = msg;
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
-      verticalPosition: 'top',      
+      verticalPosition: 'top',
     })
+  }
+
+  create(product: Product): Observable<Product>{
+    return this.http.post<Product>(this.baseUrl, product);
   }
 }
